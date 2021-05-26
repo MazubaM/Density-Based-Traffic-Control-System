2353d142d686b255ef3548a023750601df593652
MazubaM <66470312+MazubaM@users.noreply.github.com>
2021-05-26 22:39:05 +0200

    Newer Version of .py file,
    The old jupyter notebook and its .py version gave me some errors with tensorflow that I created an alternate version that doesn't require google colab or tensorflow

--- DBTCS.py
+++ DBTCS.py
@@ -1,108 +1,108 @@
 import cv2
-import matplotlib.pyplot as plt
-import cvlib as cv
-from cvlib.object_detection import draw_bbox
-import threading
-from IPython import get_ipython
+import numpy as np
 from operator import itemgetter
+from trafficlight import *
 
-def lane_1():
-    #read image from local folder/directory
-    im = cv2.imread("/content/drive/My Drive/Colab_Data/traff.jpg")
+def ImgProcess(images):
+    # Load Yolo
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()]
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-    plt.imshow(im)
-    plt.show(None)
+    # Loading image
+    img = cv2.imread(images)
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
 
-    bbox, label, conf = cv.detect_common_objects(im)
-    output_image = draw_bbox(im, bbox, label, conf)
-    plt.imshow(output_image)
-    plt.show()
-    print('Number of cars in lane 1 is ' + str(label.count('car')))
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(output_layers)
 
-    global num1
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.6:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
 
-    num1 = int(str(label.count('car')))
-  
-def lane_2():
-    #read image from local folder/directory
-    im = cv2.imread("/content/drive/My Drive/Colab_Data/traff5.jpg")
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
 
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
 
-    plt.imshow(im)
-    plt.show(None)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    num4 = 0
+    num41 = 0
+    num42 = 0
+    CarTotal = 0
 
-    bbox, label, conf = cv.detect_common_objects(im)
-    output_image = draw_bbox(im, bbox, label, conf)
-    plt.imshow(output_image)
-    plt.show()
-    print('Number of cars in lane 2 is ' + str(label.count('car')))
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+            cv2.putText(img, label, (x, y + 30), font, 0.5, color, 0)
+            
 
-    global num2
+            # global num4
+        
+            num4 += label.count('car')
+            num41 += label.count('truck')
+            num42 += label.count('motorbike')
 
-    num2 = int(str(label.count('car')))
+            CarTotal = num4 + num41 + num42
 
-def lane_3():
-    #read image from local folder/directory
-    im = cv2.imread("/content/drive/My Drive/Colab_Data/traff6.jpg")
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    plt.imshow(im)
-    plt.show(None)
-
-    bbox, label, conf = cv.detect_common_objects(im)
-    output_image = draw_bbox(im, bbox, label, conf)
-    plt.imshow(output_image)
-    plt.show()
-    print('Number of cars in lane 3 is ' + str(label.count('car')))
-
-    global num3
-
-    num3 = int(str(label.count('car')))
-
-def lane_4():
-    #read image from local folder/directory
-    im = cv2.imread("/content/drive/My Drive/Colab_Data/traff7.jpg")
-
-    plt.imshow(im)
-    plt.show(None)
-
-    bbox, label, conf = cv.detect_common_objects(im)
-    output_image = draw_bbox(im, bbox, label, conf)
-    plt.imshow(output_image)
-    plt.show()
-    print('Number of cars in lane 4 is ' + str(label.count('car')))
-
-    global num4
-    
-    num4 = int(str(label.count('car')))
-
-lane_1()
-lane_2()
-lane_3()
-lane_4()
+    return CarTotal
 
 
+lane1 = ImgProcess("traff5.jpg")
+lane2 = ImgProcess("traff4.jpg")
+lane3 = ImgProcess("traff6.jpg")
+lane4 = ImgProcess("em.jpeg")
 
+total = lane1 + lane2 + lane3 + lane4
 
 def time():
       global total, total_time, lane1_time, lane2_time, lane3_time, lane4_time 
-
-      total = num1 + num2 + num3 + num4
       
-      lane1_time = (num1/total) * 60
-      lane2_time = (num2/total) * 60
-      lane3_time = (num3/total) * 60
-      lane4_time = (num4/total) * 60
+      lane1_time = (lane1/total) * 60
+      lane2_time = (lane2/total) * 60
+      lane3_time = (lane3/total) * 60
+      lane4_time = (lane4/total) * 60
 
-      total_time = lane1_time + lane2_time + lane3_time + lane4_time
+      total_time = round(lane1_time) + round(lane2_time) + round(lane3_time) + round(lane4_time)
 
 time()
 
 def queue():
 
-      list = [{'Lane': 1, 'No_of_Cars': num1, 'Time' : round(lane1_time)},
-              {'Lane': 2, 'No_of_Cars': num2, 'Time' : round(lane2_time)},
-              {'Lane': 3, 'No_of_Cars': num3, 'Time' : round(lane3_time)},
-              {'Lane': 4, 'No_of_Cars': num4, 'Time' : round(lane4_time)}]
+      list = [{'Lane': 1, 'No_of_Cars': lane1, 'Time' : round(lane1_time)},
+              {'Lane': 2, 'No_of_Cars': lane2, 'Time' : round(lane2_time)},
+              {'Lane': 3, 'No_of_Cars': lane3, 'Time' : round(lane3_time)},
+              {'Lane': 4, 'No_of_Cars': lane4, 'Time' : round(lane4_time)}]
 
       sorted_list = sorted(list, key=itemgetter('No_of_Cars'))
 
@@ -111,9 +111,8 @@ def queue():
 
       
       print("Total Number of Cars is: ", total)
-      print("Complete Cycle time is: ", total_time)
+      print("Complete Cycle time is: ", round(total_time))
 
 queue()
 
-#delete variables from memory
-del total, total_time, lane1_time, lane2_time, lane3_time, lane4_time
+timecalc() 
